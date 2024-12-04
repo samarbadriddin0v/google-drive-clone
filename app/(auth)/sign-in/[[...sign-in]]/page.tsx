485855c7c164aa70d3be119cc0ea7c87e8133153
samarbadriddin0v <sammibadriddinov@gmail.com>
2024-12-04 08:45:46 +0200

    Fix: Middleware

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,17 +1,17 @@
-"use client";
+'use client'
 
-import { SignIn } from "@clerk/nextjs";
-import { dark, shadesOfPurple } from "@clerk/themes";
-import { useTheme } from "next-themes";
+import { SignIn } from '@clerk/nextjs'
+import { dark, shadesOfPurple } from '@clerk/themes'
+import { useTheme } from 'next-themes'
 
 export default function Page() {
-  const { resolvedTheme } = useTheme();
+	const { resolvedTheme } = useTheme()
 
-  return (
-    <SignIn
-      appearance={{
-        baseTheme: resolvedTheme === "dark" ? dark : shadesOfPurple,
-      }}
-    />
-  );
+	return (
+		<SignIn
+			appearance={{
+				baseTheme: resolvedTheme === 'dark' ? dark : shadesOfPurple,
+			}}
+		/>
+	)
 }
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,25 @@
-import Navbar from "@/components/shared/navbar";
-import Sidebar from "@/components/shared/sidebar";
-import React from "react";
+import Navbar from '@/components/shared/navbar'
+import Sidebar from '@/components/shared/sidebar'
+import { auth } from '@clerk/nextjs'
+import { redirect } from 'next/navigation'
+import React from 'react'
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div>
-      <Navbar />
-      <Sidebar />
-      <main className="w-full min-h-[90vh] relative top-[10vh] pl-72 bg-[#F6F9FC] dark:bg-[#1f1f1f] pr-4">
-        <div className="min-h-[90vh] rounded-xl bg-white dark:bg-black p-4">
-          {children}
-        </div>
-      </main>
-    </div>
-  );
-};
+const RootLayout = async ({ children }: { children: React.ReactNode }) => {
+	const { userId } = await auth()
 
-export default RootLayout;
+	if (!userId) return redirect('/sign-in')
+
+	return (
+		<div>
+			<Navbar />
+			<Sidebar />
+			<main className='w-full min-h-[90vh] relative top-[10vh] pl-72 bg-[#F6F9FC] dark:bg-[#1f1f1f] pr-4'>
+				<div className='min-h-[90vh] rounded-xl bg-white dark:bg-black p-4'>
+					{children}
+				</div>
+			</main>
+		</div>
+	)
+}
+
+export default RootLayout
