4027889d909d5a9d44a5a9e3ec7d9073c9dd9bd3
Samar Badriddinov <146908871+samarbadriddin0v@users.noreply.github.com>
2024-12-16 06:39:44 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,24 @@ import SubscriptionProvider from "@/components/providers/subscription-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Google drive",
-  description: "Google drive clone built with Next.js and Tailwind CSS",
-  icons: { icon: "/logo.svg" },
-};
+	metadataBase: new URL('https://drive.sammi.ac'),
+	title: 'Google drive',
+	description: 'Google drive web application clone created by Sammi.ac',
+	authors: [{ name: 'Samar Badriddinov', url: 'https://drive.sammi.ac' }],
+	icons: { icon: '/logo.svg' },
+	openGraph: {
+		title: 'Google drive',
+		description: "Google drive web application clone created by Sammi.ac",
+		type: 'website',
+		url: 'https://drive.sammi.ac',
+		locale: 'uz_UZ',
+		images: 'https://media.graphassets.com/urB1Sz4bTSmunkuO4Ujn',
+		countryName: 'Uzbekistan',
+		siteName: 'Sammi',
+		emails: 'info@sammi.ac',
+	},
+	keywords: "Google Drive, Google drive web, Google drive clone, Google drive web application, samar badriddinov"
+}
 
 export default function RootLayout({
   children,
